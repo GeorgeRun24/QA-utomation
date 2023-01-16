5ab52c0ecf3088a836387814e45354232d96bda1
GeorgeRun24 <x_zero24@hotmail.com>
2023-01-16 01:26:18 -0600

    Updating scientific calculator

--- calculator.py
+++ calculator.py
@@ -22,6 +22,19 @@ def power(x, y):
     a = pow(x, y)
     return a
 
+
+# This function root number
+def root(x):
+    return(math.sqrt(x))  
+
+# This function sin number
+def sin(x):
+    return (math.sin(x)) 
+
+# This function cos number
+def cos(x):
+    return (math.cos(x))   
+
 #Instructions for the calculator
 print("-----> Iniciando calculadora...")
 print("")
@@ -30,12 +43,15 @@ print("Presione (2) para restar")
 print("Presione (3) para multiplicar")
 print("Presione (4) para dividir")
 print("Presione (5) para exponente")
+print("Presione (6) para raiz")
+print("Presione (7) para seno")
+print("Presione (8) para coseno")
 print("Presione (0) para apagar")
 print("")
 
 while True:
     # take input from the user
-    choice = input("Selecciona la opción deseada (1/2/3/4/5/0): ")
+    choice = input("Selecciona la opción deseada (1/2/3/4/5/6/7/8/0): ")
 
     # check if choice is one of the nine options
     if choice in ('1', '2', '3', '4', '5'):
@@ -60,6 +76,21 @@ while True:
 
         elif choice == '5':
             print(num1, "/", num2, "=", pow(num1, num2))
+    
+    elif choice in ('6', '7', '8'):
+        try:
+            num1 = float(input("Ingresa primer número: "))
+        except ValueError:
+            print("Ingresa una opción valida :) ")
+            continue
+        if choice == '6':
+            print("La raíz de",num1, "es", root(num1))
+
+        elif choice == '7':
+            print("El seno de", num1, "es", sin(num1))
+
+        elif choice == '8':
+            print("El coseno de", num1, "es", cos(num1))
     elif choice in ('0'):
         if choice == '0':
             quit() 
